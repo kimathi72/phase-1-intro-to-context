f8e1f50d5e6a765c1696c1cab32dd75a279149c5
roy kimathi <kimathiwaweru@gmail.com>
2023-04-14 13:43:05 +0300

    done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
 // Your code here
+const createEmployeeRecord = function (array){
+    const employeeRecord = {};
+    employeeRecord.firstName = array[0];
+    employeeRecord.familyName = array[1];
+    employeeRecord.title = array[2];
+    employeeRecord.payPerHour = array[3];
+    employeeRecord.timeInEvents = [];
+    employeeRecord.timeOutEvents = [];
+    return employeeRecord;
+};
+const createEmployeeRecords = (array) => {
+    return array.map(item => createEmployeeRecord(item));
+        
+}
+const createTimeInEvent = (object, string) => {
+    let array = string.split(" ");
+    const obj = {};
+    obj.type = "TimeIn";
+    obj.date = array[0];
+    obj.hour = Number(array[1]);
+    object["timeInEvents"].push(obj);
+    return object;
+}
+const createTimeOutEvent = (object, string) => {
+    let array = string.split(" ");
+    const obj = {};
+    obj.type = "TimeOut";
+    obj.date = array[0];
+    obj.hour = Number(array[1]);
+    object["timeOutEvents"].push(obj);
+    return object;
+}
+const hoursWorkedOnDate = (object, string) => {
+    return((object.timeOutEvents.find(item => item.date === string).hour) - (object.timeInEvents.find(item => item.date === string).hour))/100;
+}
+const wagesEarnedOnDate = (object, string) => {
+    return hoursWorkedOnDate(object , string) * object.payPerHour;
+}
+const allWagesFor = (object) => {
+    const datesWorked = object.timeInEvents.map(item => item.date);
+    const wagesArray = datesWorked.map(item => {
+        return wagesEarnedOnDate(object, item);
+    })
+    const totalWages = wagesArray.reduce(function(accum, element){
+        return accum += element;
+    })
+    return totalWages;
+}
+const calculatePayroll = (object) => {
+    return object.map(item => allWagesFor(item)).reduce(function(accum,element){
+        return accum += element;
+    });
+}
